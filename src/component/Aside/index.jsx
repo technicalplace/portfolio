3e7f05ead7d09b01f97cd4782be4aff7cefb90e2
Shunsuke Fujinawa <shun26497204@gmail.com>
2026-09-24 18:20:37 +0900

    Githubとスキルシートのアイコンを追加

--- src/component/Aside/index.jsx
+++ src/component/Aside/index.jsx
@@ -2,6 +2,8 @@ import styles from './index.module.css';
 import { IoAccessibility, IoHomeOutline } from 'react-icons/io5';
 import { MdLaptopChromebook } from 'react-icons/md';
 import { IoIosMail } from 'react-icons/io';
+import { FaGithub } from 'react-icons/fa';
+import { SiGooglesheets } from 'react-icons/si';
 
 export const Aside = () => {
   const pageReload = () => {
@@ -10,8 +12,8 @@ export const Aside = () => {
 
   return (
     <aside className={styles.aside}>
-      <a href="/" className={styles.homeLink} onClick={pageReload}>
-        <IoHomeOutline className={styles.homeIcon} size={26} aria-hidden="true" />
+      <a href="/" className={styles.homeLink} onClick={pageReload} aria-label="リロード">
+        <IoHomeOutline className={styles.homeIcon} size={26} />
         <div className={styles.titleWrapper}>
           <p className={styles.position}>Frontend Engineer</p>
           <p className={styles.name}>Shunsuke Fujinawa</p>
@@ -36,6 +38,25 @@ export const Aside = () => {
             <IoIosMail size={24} />
             <p>Contact</p>
           </button>
+        </a>
+      </div>
+
+      <div className={styles.externalLinks}>
+        <a
+          href="https://github.com/technicalplace"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="GitHubを開く"
+        >
+          <FaGithub size={28} />
+        </a>
+        <a
+          href="https://docs.google.com/spreadsheets/d/1tuN_xRYs2BgH3zux7AC-aowcwe6rh2wVfIKsaVFn1Zc/edit?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="スキルシートを開く"
+        >
+          <SiGooglesheets size={28} />
         </a>
       </div>
     </aside>
